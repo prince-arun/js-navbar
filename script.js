bd392edfe329357bf6cfe437d41acaec8bd4cdcf
prince-arun <prince.skillsafari@gmail.com>
2023-06-09 02:16:35 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button");
+document.addEventListener("click", () => {
+  const menu = document.querySelector("#custom-menu");
+  menu.classList.toggle("custom-menu-toggle");
+});
